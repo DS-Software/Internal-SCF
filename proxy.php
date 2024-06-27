b99fca13d4b3c505a96fe37bb51fcfde6311c7dc
DS Software <artemsoftware@mail.ru>
2024-06-27 15:20:32 +0400

    debug: error logging

--- proxy.php
+++ proxy.php
@@ -1,4 +1,9 @@
 <?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
     $host = "http://95.217.33.122";
     $port = $_GET['port'];
     $path = $_GET['path'];
